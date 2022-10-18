3ad70ff3b02476ea39a59a72a58c1638efa49867
Alex DiCarlo <alexdicarlo.biz@gmail.com>
2022-10-18 23:38:32 +0100

    finished nav section

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 
-const search = document.getElementById('search-bar')
-const exampleDiv = document.querySelector('.example')
+// const search = document.getElementById('search-bar')
+// const exampleDiv = document.querySelector('.example')
 
 
-search.addEventListener('input', (e) => {
-  if (e.target.value.length >= 3) {
-    buildDOM()
+// search.addEventListener('input', (e) => {
+//   if (e.target.value.length >= 3) {
+//     buildDOM()
+//   }
+// })
+
+// const buildDOM = () => {
+//    exampleDiv.innerHTML = "<h2> Test </h2> "
+
+// }
+
+//
+
+const burgerMenu = document.querySelector('.burger-menu')
+const categoryDropDown = document.querySelector('.category-list')
+
+burgerMenu.addEventListener('click', (e) => {
+  if (categoryDropDown.classList.value === 'category-list') {
+      categoryDropDown.classList.remove('category-list')
+      categoryDropDown.classList.add('category-list-block')
+  } else {
+    categoryDropDown.classList.remove('category-list-block')
+    categoryDropDown.classList.add('category-list')
   }
+
 })
 
-const buildDOM = () => {
-   exampleDiv.innerHTML = "<h2> Test </h2> "
+const shopButtons = document.querySelectorAll('.btn-sale')
+let spanTag = document.getElementsByTagName('span')
+// console.log(spanTag[0].innerHTML)
 
-}
+shopButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const parsed = parseInt(spanTag[0].innerHTML)
+    spanTag[0].innerHTML = parsed + 1
+  })
+})
